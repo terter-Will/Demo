bc247cf45cceeec11655777b36d53527f3040ddd
terter-Will <es9211666@gmail.com>
2023-07-10 22:08:31 +0800

    deploy

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
-import 'tailwindcss/tailwind.css'
 
 createApp(App).use(router).mount('#app')
 
