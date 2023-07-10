d4992aaa5c8bd5c8422f5b15c43bff65bbabd405
terter-Will <es9211666@gmail.com>
2023-07-10 22:40:35 +0800

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/demo/"
+  base: "/demo/",
+  build: {
+    rollupOptions: {
+      external: ['vue-router'],
+    },
+  },
 })
